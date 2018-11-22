958d7359516279d2d76f2ecb2bcbce4d68f6e473
Rocky Wu <rwu823@gmail.com>
2018-11-22 22:40:33 +0800

    fix server lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,5 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'object-curly-spacing': 0,
-    'no-console': 0,
-    'global-require': 0,
-    'no-unused-expressions': [2, { allowTaggedTemplates: true }],
-    'consistent-return': 0,
-    'no-underscore-dangle': 0,
-    'no-debugger': 0,
-    'no-param-reassign': [2, { props: false }],
-    'no-unused-vars': [
-      2,
-      { vars: 'all', args: 'none', ignoreRestSiblings: true },
-    ],
-
-    'prettier/prettier': 2,
-
-    'react/jsx-curly-brace-presence': [
-      2,
-      { props: 'always', children: 'never' },
-    ],
-    'react/no-unescaped-entities': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prefer-stateless-function': 0,
-    'react/prop-types': [
-      2,
-      {
-        ignore: ['children', 'style', 'location', 'className'],
-      },
-    ],
-
-    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
   },
 };
