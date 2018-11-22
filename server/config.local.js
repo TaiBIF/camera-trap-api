df4992f70093645c2519fcdb84aed9c6715ed7fb
Rocky Wu <rwu823@gmail.com>
2018-11-22 09:40:08 +0800

    Fix api base

--- server/config.local.js
+++ server/config.local.js
@@ -1,10 +1,12 @@
-let p = require('../package.json');
-let version_splits = p.version.split('.');
-let version = version_splits[0] + '.' + version_splits[1];
+const p = require('../package.json');
+
+const [major, minor] = p.version.split('.');
+const version = `${major}.${minor}`;
+
 module.exports = {
   // restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
-  restApiRoot: '/api' + '/v' + version,
-  baseRestApiRoot: '/api',
+  restApiRoot: `/v${version}`,
+  baseRestApiRoot: '/',
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 };
