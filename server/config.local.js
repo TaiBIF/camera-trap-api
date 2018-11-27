699b01c26c702063fb54ad3c3a4532fb702b0335
Chihjen Ko <cjk.halodule@gmail.com>
2018-11-27 14:36:35 +0800

    Removed dependency on cjk local settings.

--- server/config.local.js
+++ server/config.local.js
@@ -1,10 +1,7 @@
 const p = require('../package.json');
-const datasources = require('./datasources.local.json');
 
 const [major, minor] = p.version.split('.');
 const version = `${major}.${minor}`;
-
-console.log('Datasource using %s', datasources.ctMongoDb40.url);
 
 module.exports = {
   // restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
