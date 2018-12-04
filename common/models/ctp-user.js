2acbc8050b5b675e548dab7e074e60e03c43b60e
Kelp <kelp@phate.org>
2018-12-04 16:28:06 +0800

    Updated login flow. #65
    POST /cpt-user/sign-in will get 302 redirect.

--- common/models/ctp-user.js
+++ common/models/ctp-user.js
@@ -1,3 +1,4 @@
+const url = require('url');
 const atob = require('atob');
 const AWS = require('aws-sdk');
 const errors = require('../errors');
@@ -7,6 +8,7 @@ const USER_POOL_ID = 'ap-northeast-1_R2iDn5W3B';
 // eslint-disable-next-line max-len
 const AWS_ID_PROVIDER = `cognito-idp.ap-northeast-1.amazonaws.com/${USER_POOL_ID}`;
 const IDENTITY_POOL_ID = 'ap-northeast-1:3d5edbfb-834c-4284-85f5-a4ec29d38ef0';
+const LOGIN_REDIRECT_HOSTNAME_WHITE_LIST = ['localhost', 'camera-trap.tw'];
 
 module.exports = function(CtpUsers) {
   CtpUsers.remoteMethod('signIn', {
@@ -15,40 +17,41 @@ module.exports = function(CtpUsers) {
     accepts: [
       { arg: 'data', type: 'object', http: { source: 'body' } },
       { arg: 'req', type: 'object', http: { source: 'req' } },
+      { arg: 'res', type: 'object', http: { source: 'res' } },
     ],
     returns: { arg: 'ret', type: 'object' },
   });
 
-  CtpUsers.signIn = function(data, req, callback) {
-    const { idToken } = data;
-    const login = {};
-    login[AWS_ID_PROVIDER] = idToken;
+  CtpUsers.signIn = function(data, req, res, callback) {
+    const { idToken, redirect } = data;
 
     AWS.config.update({ region: AWS_REGION });
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
       IdentityPoolId: IDENTITY_POOL_ID,
-      Logins: login,
+      Logins: (() => {
+        const result = {};
+        result[AWS_ID_PROVIDER] = idToken;
+        return result;
+      })(),
     });
 
     AWS.config.credentials.get(err => {
       if (err) {
-        callback(err);
-        throw err;
-      } else {
-        // 成功透過 OAuth 登入 AWS Cognito，取得 identity id
+        return callback(err);
+      }
+      // 成功透過 OAuth 登入 AWS Cognito，取得 identity id
+      const idT = AWS.config.credentials.params.Logins[AWS_ID_PROVIDER];
+      const payload = idT.split('.')[1];
+      const tokenobj = JSON.parse(atob(payload));
+      const userId = tokenobj['cognito:username'];
 
-        const idT = AWS.config.credentials.params.Logins[AWS_ID_PROVIDER];
-        const payload = idT.split('.')[1];
-        const tokenobj = JSON.parse(atob(payload));
-        const userId = tokenobj['cognito:username'];
-
-        CtpUsers.getDataSource().connector.connect((_err, db) => {
-          if (_err) {
-            return callback(_err);
-          }
-          const dateTime = Date.now() / 1000;
-          const mdl = db.collection('CtpUser');
-          mdl.updateOne(
+      CtpUsers.getDataSource().connector.connect((_err, db) => {
+        if (_err) {
+          return callback(_err);
+        }
+        const dateTime = Date.now() / 1000;
+        db.collection('CtpUser')
+          .updateOne(
             { _id: userId },
             {
               $set: {
@@ -75,21 +78,31 @@ module.exports = function(CtpUsers) {
             {
               upsert: true,
             },
-          );
-        });
-
-        const userInfo = {
-          userId,
-          // eslint-disable-next-line camelcase
-          identity_id: AWS.config.credentials.identityId,
-          name: tokenobj.name,
-          idToken: tokenobj,
-        };
-
-        // eslint-disable-next-line camelcase
-        req.session.user_info = userInfo;
-        callback(null, userId);
-      }
+          )
+          .then(() => {
+            const userInfo = {
+              userId,
+              // eslint-disable-next-line camelcase
+              identity_id: AWS.config.credentials.identityId,
+              name: tokenobj.name,
+              idToken: tokenobj,
+            };
+            // eslint-disable-next-line camelcase
+            req.session.user_info = userInfo;
+            const redirectUrl = url.parse(redirect);
+            if (
+              LOGIN_REDIRECT_HOSTNAME_WHITE_LIST.indexOf(
+                redirectUrl.hostname,
+              ) >= 0
+            ) {
+              return res.redirect(redirect);
+            }
+            callback(null, userId);
+          })
+          .catch(error => {
+            callback(error);
+          });
+      });
     });
   };
 
