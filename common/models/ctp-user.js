1b6df465568bbfd1af9f1eacc14fee6b945cff11
Jason Guan Shuo Mai <trashmai@gmail.com>
2018-12-05 09:17:38 +0800

    make every demo user ProjectManager of 測試計畫1

--- common/models/ctp-user.js
+++ common/models/ctp-user.js
@@ -76,6 +76,12 @@ module.exports = function(CtpUsers) {
                     projectTitle: 'ANY_NEW',
                     roles: ['ProjectInitiator'],
                   },
+                  // 2018-12-05 demo 用, 結束後請移除
+                  {
+                    projectId: 'd8064aa7-9643-44fb-bed9-1f23a690f968',
+                    projectTitle: '測試計畫1',
+                    roles: ['ProjectManager'],
+                  },
                 ],
               },
             },
