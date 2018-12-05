63d93df57823097f99760ebbce038cdbbc0ca607
Rocky Wu <rwu823@gmail.com>
2018-12-05 20:33:00 +0800

    Add deploy to eb prod

--- scripts/aws-deploy-start.ts
+++ scripts/aws-deploy-start.ts
@@ -1,6 +1,6 @@
 import slack from './slack'
 
-const { CIRCLE_SHA1 } = process.env;
+const { CIRCLE_SHA1, CIRCLE_BRANCH } = process.env;
 
 (async () => {
   const sha1 = CIRCLE_SHA1!.slice(0 , 6);
@@ -11,7 +11,7 @@ const { CIRCLE_SHA1 } = process.env;
     icon_emoji: ':circleci:',
     attachments: [
       {
-        text: `<https://github.com/TaiBIF/camera-trap-api/commit/${sha1}|${sha1}> Deploy to AWS Elastic Beanstalk is starting...`,
+        text: `\`[${CIRCLE_BRANCH}]\` <https://github.com/TaiBIF/camera-trap-api/commit/${sha1}|${sha1}> Deploy to AWS Elastic Beanstalk is starting...`,
       }
     ]
   });
