cd5a232e237035660b74723c257432f5712164f5
Chihjen Ko <cjk.halodule@gmail.com>
2018-12-16 18:39:48 +0800

    NODE_ENV=local environment tweak.

--- server/config.local.js
+++ server/config.local.js
@@ -1,21 +1,9 @@
 const p = require('../package.json');
 
-// for development using local db, if exists.
-if (process.env.NODE_ENV === 'local') {
-  try {
-    const datasources = require('./datasources.local.json');
-    console.log(`Using local data source at ${datasources.ctMongoDb40.url}`);
-  } catch (err) {
-    console.log('Local data sources settings not found.');
-    console.log(err.code);
-  }
-}
-
 const [major, minor] = p.version.split('.');
 const version = `${major}.${minor}`;
 
 module.exports = {
-  // restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
   restApiRoot: `/v${version}`,
   baseRestApiRoot: '/',
   host: process.env.HOST || 'localhost',
