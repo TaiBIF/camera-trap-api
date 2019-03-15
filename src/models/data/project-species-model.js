301630cdfb8c131836466496c303dad8db5f7a58
Kelp <kelp@phate.org>
2019-03-15 16:46:22 +0800

    Added ProjectSpeciesModel.

---
+++ src/models/data/project-species-model.js
@@ -0,0 +1,51 @@
+const { Schema } = require('mongoose');
+const utils = require('../../common/utils');
+const ProjectSpeciesCode = require('../const/project-species-code');
+
+const db = utils.getDatabaseConnection();
+const model = db.model(
+  'ProjectSpeciesModel',
+  utils.generateSchema(
+    {
+      project: {
+        type: Schema.ObjectId,
+        ref: 'ProjectModel',
+        required: true,
+        index: {
+          name: 'Project',
+        },
+      },
+      title: {
+        // 物種名稱
+        'zh-TW': {
+          // 繁體中文
+          type: String,
+        },
+      },
+      index: {
+        // 排序 (由小到大)
+        type: Number,
+        default: 0,
+      },
+      code: {
+        // 供「提示定義」使用
+        type: String,
+        enum: ProjectSpeciesCode.all(),
+      },
+    },
+    {
+      collection: 'ProjectSpecies',
+    },
+  ),
+);
+
+model.prototype.dump = function() {
+  return {
+    id: `${this._id}`,
+    title: this.title,
+    index: this.index,
+    code: this.code,
+  };
+};
+
+module.exports = model;
