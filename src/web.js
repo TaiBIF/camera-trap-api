7bc734dda1f2403bdc1b9c7c012736c25c31b7f7
Kelp <kelp@phate.org>
2019-03-21 14:43:08 +0800

    Added error handler.

--- src/web.js
+++ src/web.js
@@ -2,6 +2,7 @@ const http = require('http');
 const config = require('config');
 const express = require('express');
 const nocache = require('nocache');
+const errors = require('./models/errors');
 const webRouter = require('./routers/web-router');
 
 const app = express();
@@ -10,8 +11,22 @@ const server = http.createServer(app);
 app.use(nocache());
 app.use('/api/v1', webRouter);
 
+// error handler
+app.use((req, res, next) => {
+  // Didn't match any routers.
+  next(new errors.Http404());
+});
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  res.statusCode = error.status;
+  res.error = error;
+  res.json({
+    message: error.message,
+  });
+});
+
 // launch server
 server.listen(config.server.port, config.server.host, () => {
-  const address = server.address();
-  console.log(`Server listening at http://${address.address}:${address.port}`);
+  const { address, port } = server.address();
+  console.log(`Server listening at http://${address}:${port}`);
 });
