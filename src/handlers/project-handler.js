0a81420696ec1becfe6059d918b79600b93c2f8c
Kelp <kelp@phate.org>
2019-03-26 16:42:18 +0800

    Insert default species of the project at POST /api/v1/projects.

--- src/handlers/project-handler.js
+++ src/handlers/project-handler.js
@@ -8,6 +8,8 @@ const ProjectsSearchForm = require('../forms/project/projects-search-form');
 const ProjectForm = require('../forms/project/project-form');
 const DataFieldModel = require('../models/data/data-field-model');
 const DataFieldSystemCode = require('../models/const/data-field-system-code');
+const SpeciesModel = require('../models/data/species-model');
+const SpeciesCode = require('../models/const/species-code');
 
 exports.getProjects = auth(UserPermission.all(), (req, res) => {
   /*
@@ -74,6 +76,38 @@ exports.addProject = auth(UserPermission.all(), (req, res) => {
       return project.save();
     })
     .then(project => {
+      // Add default species.
+      const species = [
+        new SpeciesModel({
+          project,
+          code: SpeciesCode.emptyShot,
+          title: {
+            'zh-TW': '空拍',
+          },
+          index: 0,
+        }),
+        new SpeciesModel({
+          project,
+          code: SpeciesCode.test,
+          title: {
+            'zh-TW': '測試',
+          },
+          index: 1,
+        }),
+        new SpeciesModel({
+          project,
+          code: SpeciesCode.people,
+          title: {
+            'zh-TW': '人',
+          },
+          index: 2,
+        }),
+      ];
+      const result = species.map(doc => doc.save());
+      result.unshift(project);
+      return Promise.all(result);
+    })
+    .then(([project]) => {
       res.json(project.dump());
     });
 });
