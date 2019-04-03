9359ee3914d15ce94399722484c2d79cec660272
Kelp <kelp@phate.org>
2019-04-03 22:51:10 +0800

    Added ExchangeableImageFileModel.

---
+++ src/models/data/exchangeable-image-file-model.js
@@ -0,0 +1,38 @@
+const utils = require('../../common/utils');
+
+const db = utils.getDatabaseConnection();
+const schema = utils.generateSchema(
+  {
+    make: {
+      // 相機製造商
+      type: String,
+    },
+    model: {
+      // 相機型號
+      type: String,
+    },
+    dateTime: {
+      // 拍照當下時間，用 GMT+8 換算成 UTC。
+      type: Date,
+    },
+    rawData: {
+      type: String,
+    },
+  },
+  {
+    collection: 'ExchangeableImageFiles',
+  },
+);
+const model = db.model('ExchangeableImageFileModel', schema);
+
+model.prototype.dump = function() {
+  return {
+    id: `${this._id}`,
+    make: this.make,
+    model: this.model,
+    dateTime: this.dateTime,
+    rawData: this.rawData,
+  };
+};
+
+module.exports = model;
