ecee9da79384e45fcf49cc836e49e28bf07dbffd
Kelp <kelp@phate.org>
2019-04-20 20:19:56 +0800

    Create upload-sessions-search-form.js

---
+++ src/forms/upload-session/upload-sessions-search-form.js
@@ -0,0 +1,30 @@
+const forms = require('../');
+
+class UploadSessionsSearchForm extends forms.Form {}
+UploadSessionsSearchForm.define({
+  index: new forms.fields.IntegerField({
+    filter: value => {
+      const result = forms.filters.integer(0)(value);
+      return result < 0 ? 0 : result;
+    },
+  }),
+  size: new forms.fields.IntegerField({
+    filter: value => {
+      const result = forms.filters.integer(forms.constants.PAGE_SIZE_MAXIMUM)(
+        value,
+      );
+      if (result < 0) {
+        return forms.constants.PAGE_SIZE_MAXIMUM;
+      }
+      if (result > forms.constants.PAGE_SIZE_MAXIMUM) {
+        return forms.constants.PAGE_SIZE_MAXIMUM;
+      }
+      return result;
+    },
+  }),
+  sort: new forms.fields.StringField({
+    filter: value => value || 'createTime',
+    validators: [forms.validators.regexp(/^-?(createTime)$/)],
+  }),
+});
+module.exports = UploadSessionsSearchForm;
