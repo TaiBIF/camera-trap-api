1bdee470d9f3332b85012a4c46c042fd07630142
Kelp <kelp@phate.org>
2019-04-21 14:26:52 +0800

    Support update fields at PUT /api/v1/annotations/:annotationId.

--- src/forms/annotation/annotation-form.js
+++ src/forms/annotation/annotation-form.js
@@ -5,5 +5,16 @@ CameraLocationForm.define({
   species: new forms.fields.StringField({
     validators: [forms.validators.id()],
   }),
+  fields: new forms.fields.ArrayField({
+    subField: new forms.fields.Field({
+      validators: [
+        (field = {}) => {
+          const idValidator = forms.validators.id();
+          const lengthValidator = forms.validators.length({ max: 1024 });
+          return idValidator(field.dataField) || lengthValidator(field.value);
+        },
+      ],
+    }),
+  }),
 });
 module.exports = CameraLocationForm;
--- src/handlers/annotation-handler.js
+++ src/handlers/annotation-handler.js
@@ -3,7 +3,8 @@ const errors = require('../models/errors');
 const PageList = require('../models/page-list');
 const UserPermission = require('../models/const/user-permission');
 const CameraLocationModel = require('../models/data/camera-location-model');
-require('../models/data/project-model'); // for populate
+const DataFieldWidgetType = require('../models/const/data-field-widget-type');
+const ProjectModel = require('../models/data/project-model');
 const StudyAreaModel = require('../models/data/study-area-model');
 const StudyAreaState = require('../models/const/study-area-state');
 const SpeciesModel = require('../models/data/species-model');
@@ -11,6 +12,7 @@ const AnnotationsSearchForm = require('../forms/annotation/annotations-search-fo
 const AnnotationForm = require('../forms/annotation/annotation-form');
 const AnnotationModel = require('../models/data/annotation-model');
 const AnnotationState = require('../models/const/annotation-state');
+const AnnotationFailureType = require('../models/const/annotation-failure-type');
 
 exports.getAnnotations = auth(UserPermission.all(), (req, res) => {
   /*
@@ -126,20 +128,25 @@ exports.updateAnnotation = auth(UserPermission.all(), (req, res) => {
   }
 
   return Promise.all([
-    AnnotationModel.findById(req.params.annotationId)
-      .where({ state: AnnotationState.active })
-      .populate('project'),
+    AnnotationModel.findById(req.params.annotationId).where({
+      state: AnnotationState.active,
+    }),
     SpeciesModel.findById(form.species),
   ])
-    .then(([annotation, species]) => {
+    .then(([annotation, species]) =>
+      Promise.all([
+        ProjectModel.findById(annotation.project).populate('dataFields'),
+        annotation,
+        species,
+      ]),
+    )
+    .then(([project, annotation, species]) => {
       if (!annotation) {
         throw new errors.Http404();
       }
       if (
         req.user.permission !== UserPermission.administrator &&
-        !annotation.project.members.find(
-          x => `${x.user._id}` === `${req.user._id}`,
-        )
+        !project.members.find(x => `${x.user._id}` === `${req.user._id}`)
       ) {
         throw new errors.Http403();
       }
@@ -147,14 +154,69 @@ exports.updateAnnotation = auth(UserPermission.all(), (req, res) => {
         if (!species) {
           throw new errors.Http404();
         }
-        if (`${species.project._id}` !== `${annotation.project._id}`) {
+        if (`${species.project._id}` !== `${project._id}`) {
           throw new errors.Http400(
             'The project of species and the project of the annotation are different.',
           );
         }
       }
+      const projectDataFields = {};
+      const formFieldIds = new Set();
+      project.dataFields.forEach(dataField => {
+        projectDataFields[`${dataField._id}`] = dataField;
+      });
+      (form.fields || []).forEach(field => {
+        if (!(field.dataField in projectDataFields)) {
+          throw new errors.Http400(
+            `Data field ${field.dataField} not in the project.`,
+          );
+        }
+        if (formFieldIds.has(field.dataField)) {
+          throw new errors.Http400(`${field.dataField} is duplicate.`);
+        }
+        formFieldIds.add(field.dataField);
+      });
 
+      // Remove newSpecies failure flag.
+      const newSpeciesIndex = annotation.failures.indexOf(
+        AnnotationFailureType.newSpecies,
+      );
+      if (newSpeciesIndex >= 0) {
+        annotation.failures.splice(newSpeciesIndex, 1);
+      }
+      // Assign species.
       annotation.species = species;
+      // Assign fields.
+      annotation.fields = (form.fields || []).map(field => {
+        const value = {};
+        switch (projectDataFields[field.dataField].widgetType) {
+          case DataFieldWidgetType.time:
+            value.time = field.value;
+            break;
+          case DataFieldWidgetType.select:
+            if (
+              !projectDataFields[field.dataField].options.find(
+                x => `${x._id}` === field.value,
+              )
+            ) {
+              throw new errors.Http400(
+                `${field.value} not in ${JSON.stringify(
+                  projectDataFields[field.dataField].options,
+                )}.`,
+              );
+            }
+            value.selectId = field.value;
+            break;
+          case DataFieldWidgetType.text:
+          default:
+            value.text = field.value;
+            break;
+        }
+        return {
+          dataField: projectDataFields[field.dataField],
+          value,
+        };
+      });
       return annotation.saveAndAddRevision(req.user);
     })
     .then(annotation => {
