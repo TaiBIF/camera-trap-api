a3d5cefe701bfc67bd57290d65286eb09d130e1d
Kelp <kelp@phate.org>
2019-05-01 21:57:32 +0800

    Refactor.

--- src/handlers/upload-session-handler.js
+++ src/handlers/upload-session-handler.js
@@ -19,7 +19,7 @@ exports.getMyUploadSession = auth(UserPermission.all(), (req, res) => {
     throw new errors.Http400(errorMessage);
   }
 
-  const query = UploadSessionModel.where({ user: `${req.user._id}` })
+  const query = UploadSessionModel.where({ user: req.user._id })
     .sort(form.sort)
     .populate('project')
     .populate('cameraLocation')
