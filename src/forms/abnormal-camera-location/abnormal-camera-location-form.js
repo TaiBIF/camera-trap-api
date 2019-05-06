da1f37903f6fd2a13d29250988faa2b457d291c0
Jack Hsieh <f2e.jack@gmail.com>
2019-05-07 05:34:15 +0800

    Added missed abnormalType validators into abnormal-camera-location-form

--- src/forms/abnormal-camera-location/abnormal-camera-location-form.js
+++ src/forms/abnormal-camera-location/abnormal-camera-location-form.js
@@ -1,4 +1,5 @@
 const forms = require('../');
+const AbnormalType = require('../../models/const/abnormal-camera-location');
 
 class AbnormalCameraLocationForm extends forms.Form {}
 
@@ -17,6 +18,9 @@ AbnormalCameraLocationForm.define({
   }),
   abnormalStartDate: new forms.fields.DateField(),
   abnormalEndDate: new forms.fields.DateField(),
+  abnormalType: new forms.fields.StringField({
+    validators: [forms.validators.anyOf(AbnormalType.all())],
+  }),
   remarks: new forms.fields.StringField({
     validators: [forms.validators.length({ max: 1024 })],
   }),
