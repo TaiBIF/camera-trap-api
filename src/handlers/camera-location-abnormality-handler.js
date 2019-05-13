2c53344a6b953bb5cf6686a540e2495ad8db0d2d
Kelp <kelp@phate.org>
2019-05-13 17:38:24 +0800

    Make sure the camera location is exists at POST /api/v1/projects/:projectId/camera-location-abnormality.

--- src/handlers/camera-location-abnormality-handler.js
+++ src/handlers/camera-location-abnormality-handler.js
@@ -2,6 +2,8 @@ const auth = require('../auth/authorization');
 const errors = require('../models/errors');
 const UserPermission = require('../models/const/user-permission');
 const ProjectModel = require('../models/data/project-model');
+const CameraLocationModel = require('../models/data/camera-location-model');
+const CameraLocationState = require('../models/const/camera-location-state');
 const CameraLocationAbnormalityModel = require('../models/data/camera-location-abnormality-model');
 const CameraLocationAbnormalityForm = require('../forms/camera-location-abnormality/camera-location-abnormality-form');
 
@@ -18,8 +20,14 @@ exports.addCameraLocationAbnormality = auth(
       throw new errors.Http400(errorMessage);
     }
 
-    return ProjectModel.findById(req.params.projectId)
-      .then(project => {
+    return Promise.all([
+      ProjectModel.findById(req.params.projectId),
+      CameraLocationModel.findById(form.cameraLocation).where({
+        project: req.params.projectId,
+        state: CameraLocationState.active,
+      }),
+    ])
+      .then(([project, cameraLocation]) => {
         if (!project) {
           throw new errors.Http404();
         }
@@ -28,6 +36,9 @@ exports.addCameraLocationAbnormality = auth(
           !project.members.find(x => `${x.user._id}` === `${req.user._id}`)
         ) {
           throw new errors.Http403();
+        }
+        if (!cameraLocation) {
+          throw new errors.Http400('The camera location is not exists.');
         }
 
         const cameraLocationAbnormality = new CameraLocationAbnormalityModel({
