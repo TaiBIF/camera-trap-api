42ea04b1adfe866d51abc0d96c5056dd897d6d2c
onetail <paosong91@gmail.com>
2019-08-02 12:16:24 +0800

    beauty and best code in error.js

--- src/models/errors-code.js
+++ src/models/errors-code.js
@@ -5,37 +5,51 @@ exports.ERROR = {
   DATA_INVALID: 'DATA_INVALID',
   LOGIN_REQUIRED: 'LOGIN_REQUIRED',
   PERMISSION_DENIED: 'PERMISSION_DENIED',
+  INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+  DEFAULT_STATUSCODE: 'DEFAULT_STATUSCODE',
 };
 
 exports.ERROR_MAP = {
   // TODO: not yet declare
   FORMAT_INVALID: {
+    status: 400,
     code: 10000,
     message: 'The request format is invalid',
   },
   DATA_NOT_FOUND: {
+    status: 404,
     code: 10001,
     message: 'The data is not found in database',
   },
   DATA_EXISTED: {
+    status: 400,
     code: 10002,
     message: 'The data has exist in database',
   },
   DATA_INVALID: {
+    status: 400,
     code: 10003,
     message: 'The data is invalid',
   },
   LOGIN_REQUIRED: {
+    status: 400,
     code: 10004,
     message: 'Please login first',
   },
   PERMISSION_DENIED: {
+    status: 403,
     code: 10005,
     message: 'You have no permission to operate',
+  },
+  INTERNAL_SERVER_ERROR: {
+    status: 500,
+    code: 10006,
+    message: 'internal server error!',
   },
 
   // default status
   DEFAULT_STATUSCODE: {
+    status: 400,
     code: 99999,
     message: 'statusCode not giving value',
   },
--- src/models/errors.js
+++ src/models/errors.js
@@ -1,47 +1,55 @@
-const { ERROR_MAP } = require('./errors-code');
+const { ERROR_MAP, ERROR } = require('./errors-code');
 
 // 1
 exports.Http400 = class Http400 extends Error {
-  constructor({ message, statusCode } = {}) {
+  constructor(
+    { message, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
     this.status = 400;
     this.message = `${message || 'bad request'}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
 
 exports.Http401 = class Http401 extends Error {
-  constructor({ message, statusCode } = {}) {
+  constructor(
+    { message, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
     this.status = 401;
     this.message = `${message || 'unauthorized'}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
 
 exports.Http403 = class Http403 extends Error {
-  constructor({ message, statusCode } = {}) {
+  constructor(
+    { message, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
     this.status = 403;
     this.message = `${message || 'permission denied'}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
 
 exports.Http404 = class Http404 extends Error {
-  constructor({ message, statusCode } = {}) {
+  constructor(
+    { message, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
     this.status = 404;
     this.message = `${message || 'not found'}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
 
@@ -49,26 +57,28 @@ exports.Http500 = class Http500 extends Error {
   /*
   Server error.
    */
-  constructor({ message, statusCode } = {}) {
+  constructor(
+    { message, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
     this.status = 500;
     this.message = `${message || 'server error'}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
 
-// 2
+// approach 2
 exports.HttpError = class HttpError extends Error {
-  constructor({ message, status, statusCode } = {}) {
+  constructor(
+    { message, status, statusCode } = {
+      statusCode: ERROR.DEFAULT_STATUSCODE,
+    },
+  ) {
     super(message);
-    this.status = status || 507;
-    this.message = statusCode
-      ? `${message || ERROR_MAP[statusCode].message}`
-      : `${message}`;
-    this.code = statusCode
-      ? ERROR_MAP[statusCode].code
-      : ERROR_MAP.DEFAULT_STATUSCODE.code;
+    this.status = status || ERROR_MAP[statusCode].status;
+    this.message = `${message || ERROR_MAP[statusCode].message}`;
+    this.code = ERROR_MAP[statusCode].code;
   }
 };
