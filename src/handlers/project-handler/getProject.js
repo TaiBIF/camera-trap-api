e4ec2fecd6ea602c1784d975ffb40c18e10773c3
Sandy Tseng <winson58kimo@gmail.com>
2019-08-06 11:51:31 +0800

    implement dwca files upload flow

---
+++ src/handlers/project-handler/getProject.js
@@ -0,0 +1,34 @@
+const config = require('config');
+const ProjectModel = require('../../models/data/project-model');
+const errors = require('../../models/errors');
+const utils = require('../../common/utils');
+
+/*
+  GET /api/v1/projects/:projectId
+*/
+module.exports = async (req, res) => {
+  const project = await ProjectModel.findById(req.params.projectId)
+    .populate('coverImageFile')
+    .populate('areas')
+    .populate('members.user')
+    .populate('dataFields');
+
+  if (!project) {
+    throw new errors.Http404();
+  }
+  if (!project.canAccessBy(req.user)) {
+    throw new errors.Http403();
+  }
+
+  let occurance = {};
+  try {
+    const folder = config.s3.folders.annotationDWCAs;
+    occurance = await utils.getS3Object(
+      `${folder}/${project._id}/occurance.csv`,
+    );
+  } catch (e) {
+    // file not exist
+  }
+
+  res.json({ ...project.dump(), dwc: occurance.LastModified });
+};
