bff3e7fc40fc5d5cd33da13eb350d09a6684ffb3
Sandy Tseng <winson58kimo@gmail.com>
2019-08-16 13:17:18 +0800

    implement logger

---
+++ src/logger.js
@@ -0,0 +1,42 @@
+const { createLogger, format, transports } = require('winston');
+
+const config = {
+  levels: {
+    error: 0,
+    debug: 1,
+    warn: 2,
+    data: 3,
+    info: 4,
+    verbose: 5,
+    silly: 6,
+    custom: 7,
+  },
+  colors: {
+    error: 'red',
+    debug: 'blue',
+    warn: 'yellow',
+    data: 'grey',
+    info: 'green',
+    verbose: 'cyan',
+    silly: 'magenta',
+    custom: 'yellow',
+  },
+};
+
+const logger = createLogger({
+  levels: config.levels,
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({
+      format: 'YYYY-MM-dd hh:mm:ss',
+    }),
+    format.align(),
+    format.splat(),
+    format.simple(),
+    format.printf(log => `${log.timestamp} [${log.level}] ${log.message}`),
+  ),
+  transports: [new transports.Console()],
+  level: 'custom',
+});
+
+module.exports = logger;
