f8dcd95928194873f1168acd03ad1ec537b57790
onetail <paosong91@gmail.com>
2019-09-24 14:10:39 +0800

    add project trip camera form and model

---
+++ src/forms/project/project-trip-study-area.form.js
@@ -0,0 +1,19 @@
+const forms = require('../');
+
+class ProjectTripStudyAreaForm extends forms.Form {}
+ProjectTripStudyAreaForm.define({
+  studyArea: new forms.fields.StringField({
+    validators: [forms.validators.id()],
+  }),
+  cameraLocations: new forms.fields.ArrayField({
+    filter: value => value || [],
+    subField: new forms.fields.Field({
+      validators: [
+        (field = {}) =>
+          // const idValidator = forms.validators.id();
+          field.projectCameras,
+      ],
+    }),
+  }),
+});
+module.exports = ProjectTripStudyAreaForm;
---
+++ src/models/data/project-trip-model.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const utils = require('../../common/utils');
+
+const { Schema } = mongoose;
+utils.connectDatabase();
+const schema = utils.generateSchema(
+  {
+    project: {
+      type: Schema.ObjectId,
+      ref: 'ProjectModel',
+      required: true,
+      index: {
+        name: 'Project',
+      },
+    },
+    sn: {
+      type: String,
+      required: true,
+      index: {
+        name: 'sn',
+      },
+    },
+    date: {
+      // 行程日期
+      type: Date,
+    },
+    studyAreas: [
+      {
+        // 行程日期
+        title: {
+          type: String,
+        },
+        studyAreaId: {
+          type: Schema.ObjectId,
+        },
+        cameraLocations: [
+          {
+            cameraLocationId: {
+              type: Schema.ObjectId,
+            },
+            cameraLocationMark: {
+              type: String,
+            },
+            projectCameras: {
+              type: Array,
+            },
+          },
+        ],
+      },
+    ],
+    mark: {
+      type: String,
+    },
+  },
+  {
+    collection: 'ProjectTrips',
+  },
+);
+schema.index(
+  { project: 1, sn: 1 },
+  {
+    name: 'ProjectTrips',
+    background: true,
+    unique: true,
+  },
+);
+
+schema.method('dump', function() {
+  return {
+    id: `${this._id}`,
+    project:
+      this.project && typeof this.project.dump === 'function'
+        ? this.project.dump()
+        : this.project,
+    sn: this.sn,
+    data: this.data,
+    studyAreas: this.studyAreas,
+  };
+});
+
+module.exports = mongoose.model('ProjectTripModel', schema);
