4cdfa6481fd91291cd936fc438b6e9f30f8b9218
Sandy Tseng <winson58kimo@gmail.com>
2019-11-23 11:36:54 +0800

    calculate capture rate

--- src/routers/web-router.js
+++ src/routers/web-router.js
@@ -350,6 +350,7 @@ apiRouter.get(
 apiRouter.get('/calculator/work-hours', calculatorHandler.workHour);
 apiRouter.get('/calculator/valid-pics', calculatorHandler.validPics);
 apiRouter.get('/calculator/events', calculatorHandler.events);
+apiRouter.get('/calculator/capture-rate', calculatorHandler.captureRate);
 apiRouter.get('/calculator/oi3', calculatorHandler.oi3);
 apiRouter.get('/calculator/ltd', calculatorHandler.calculateLTD);
 apiRouter.get('/calculator/oi', calculatorHandler.calculateOI);
