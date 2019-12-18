d49f9dbc869fc2a9b2c60bcfc7d3d8df62c98820
MooGoo <moogoo78@gmail.com>
2019-12-19 04:27:01 +0800

    add annotation tag

--- src/handlers/annotation-handler/updateAnnotation.js
+++ src/handlers/annotation-handler/updateAnnotation.js
@@ -61,7 +61,7 @@ module.exports = async (req, res) => {
   const annotation = await AnnotationModel.findById(req.params.annotationId)
     .where({ state: AnnotationState.active })
     .populate('file');
-
+  console.log(form);
   const species =
     (await SpeciesModel.where({
       'title.zh-TW': form.speciesTitle,
@@ -109,6 +109,10 @@ module.exports = async (req, res) => {
 
   // annotation.fields = [];
 
+  if (req.body.tags) {
+    annotation.tags = req.body.tags;
+  }
+  console.log(annotation, '---------');
   await annotation.save();
 
   const projectFieldsObject = keyBy(project.dataFields, '_id');
